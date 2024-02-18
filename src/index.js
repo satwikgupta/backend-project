b7f52659ef09831cc0d63c16bd30351e0b67d3aa
Satwik Gupta <satwikgupta1801@gmail.com>
2024-02-19 02:32:30 +0530

    route and controller

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 import { app } from "./app.js"
-import { log } from "console"
 
 dotenv.config({ path: "./.env" })
 
@@ -9,6 +8,7 @@ connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log("Server is running on port: " + process.env.PORT)
+      console.log("Open the sever on https://localhost:" + process.env.PORT)
     })
   })
   .catch((err) => {
